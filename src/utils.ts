aaa8b66876d29f922580f8bcc853998ed1805303
Tim Layton <tmlayton@users.noreply.github.com>
2022-06-03 01:04:27 -0700

    switch to nodemailer

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,7 @@
-import Vonage from '@vonage/server-sdk';
-import { apiKey, apiSecret, from, to } from '../data.json';
 import tcpp from 'tcp-ping';
+import nodemailer from 'nodemailer';
+import { appPass } from '../data.json';
 
-const vonage = new Vonage({ apiKey, apiSecret });
 let offline = false;
 
 export function ping() {
@@ -10,7 +9,11 @@ export function ping() {
   tcpp.ping({ address: '192.168.1.86', attempts: 1 }, function (_, data) {
     const error = data.results[0].err;
     if (error != null) {
-      if (Object.keys(error).length === 0 || (error as any).code === 'EHOSTDOWN' || (error as any).code === 'EHOSTUNREACH') {
+      if (
+        Object.keys(error).length === 0 ||
+        (error as any).code === 'EHOSTDOWN' ||
+        (error as any).code === 'EHOSTUNREACH'
+      ) {
         offlineHandler();
       } else if ((error as any).code === 'ECONNREFUSED') {
         onlineHandler();
@@ -25,7 +28,7 @@ function offlineHandler() {
   log('Offline.');
   if (offline) return;
   offline = true;
-  sendText('⚠️ Heads-Up: The garage fridge has lost power.');
+  sendEmail();
 }
 
 function onlineHandler() {
@@ -33,18 +36,27 @@ function onlineHandler() {
   log('Online.');
 }
 
-function sendText(text: string) {
-  vonage.message.sendSms(from, to, text, {}, (err, responseData) => {
-    if (err) {
-      log(err);
+function sendEmail() {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'laytontm@gmail.com',
+      pass: appPass,
+    },
+  });
+
+  const mailOptions = {
+    from: 'laytontm@gmail.com',
+    to: 'laytontm@gmail.com',
+    subject: '⚠️ Heads-Up: The garage fridge has lost power.',
+    text: 'The Raspberry Pi appears to be offline. Please check the garage fridge.',
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
     } else {
-      if (responseData.messages[0]['status'] === '0') {
-        log('Message sent successfully.');
-      } else {
-        log(
-          `Message failed with error: ${responseData.messages[0]['error-text']}`
-        );
-      }
+      console.log('Email sent: ' + info.response);
     }
   });
 }
